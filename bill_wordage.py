ee87ba0316b5db0394e42b3fb60d8b383f0a1c72
Peter Cybriwsky <peter@d-172-25-255-169.dhcp.virginia.edu>
2017-11-03 13:43:03 -0400

    Updated lists

--- bill_wordage.py
+++ bill_wordage.py
@@ -1,17 +1,41 @@
 __author__ = 'Winston Frick & Peter Cybriwsky'
 # This snippet gets the final verdict of a bill in gettting passed or not
-file = open("va_bill_actions.csv")
-file2 = open("final_bill_wordage.txt","w")
+file = open("va_bill_actions.csv", "r")
+file2 = open("final_bill_wordage.txt", "w")
+file3 = open("va_bill_sponsors.csv", "r")
+file4 = open("va_legislators.csv", "r")
+totalbills = open("legislator_counts.txt", "w")
 bill = 0
 print(file.readline())
+legislators = {}
+IDs = []
 
 output = ""
 for line in file:
-	attributes = line.split(",")
-	current_bill = attributes[3]
-	if current_bill != bill:
-		file2.write(output + "\n")
-	output = attributes[5] + " " + attributes[6]
-	bill = current_bill
+    attributes = line.split(",")
+    current_bill = attributes[3]
+    if current_bill != bill:
+        file2.write(output + "\n")
+    output = attributes[5] + " " + attributes[6]
+    bill = current_bill
+
+for line in file4:
+    attributes = line.split(",")
+    legislators[attributes[0]] = 0
+    IDs.append(attributes[0])
+
+print(IDs)
+
+for line in file3:
+    attributes = line.strip().split(",")
+    bill = attributes[3]
+    sponsor = attributes[5]
+    if sponsor in legislators.keys():
+        legislators[sponsor] += 1
+
+for k, v in legislators.items():
+    print(k, v)
+
+print(legislators)
 
 file2.close()
